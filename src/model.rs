f1f73642dab933370e508f879487219c5fc0eaa6
Brandon Piña <brandonpi265@gmail.com>
2022-11-29 22:40:52 -0600

    Hoo boy does this need some work

--- src/model.rs
+++ src/model.rs
@@ -1,22 +1,23 @@
+use errno::Errno;
+pub use libvmaf_sys::VmafModelConfig;
+use libvmaf_sys::{vmaf_model_destroy, vmaf_model_load, VmafModel, VmafModelFlags};
 use std::{
-    ffi::c_char,
+    ffi::{c_char, CString},
     mem,
     ops::{Deref, DerefMut},
 };
-use errno::Errno;
-pub use libvmaf_sys::VmafModelConfig;
-use libvmaf_sys::{
-    vmaf_model_destroy, vmaf_model_load, VmafModel, VmafModelFlags};
 
 pub struct Model(*mut VmafModel);
 
 impl Model {
-    pub fn new(config: VmafModelConfig, version: c_char) -> Result<Model, Errno> {
+    pub fn new(config: VmafModelConfig, version: String) -> Result<Model, Errno> {
         let mut ptr: *mut VmafModel =
             unsafe { libc::malloc(mem::size_of::<VmafModel>()) as *mut VmafModel };
         let mut config = config.clone();
 
-        let err = unsafe { vmaf_model_load(&mut ptr, &mut config, &version) };
+        let version_cstring: CString = CString::new(version).unwrap();
+        let version_ptr: *const c_char = version_cstring.as_ptr() as *const c_char;
+        let err = unsafe { vmaf_model_load(&mut ptr, &mut config, version_ptr) };
 
         match err {
             0 => Ok(Model(ptr)),
@@ -51,19 +52,24 @@ impl Drop for Model {
     fn drop(&mut self) {
         unsafe {
             vmaf_model_destroy(self.0);
-            assert!(self.0.is_null())
+            self.0 = std::ptr::null_mut();
+            assert!(self.0.is_null());
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use libvmaf_sys::VmafModelConfig;
+    use libvmaf_sys::{VmafModelConfig, VmafModelFlags};
 
     use super::Model;
 
     #[test]
     fn construct() {
-        todo!()
+        let config: VmafModelConfig = VmafModelConfig {
+            name: std::ptr::null(),
+            flags: VmafModelFlags::VMAF_MODEL_FLAGS_DEFAULT as u64,
+        };
+        let _model: Model = Model::new(config, "vmaf_v0.6.1".to_string()).unwrap();
     }
 }
