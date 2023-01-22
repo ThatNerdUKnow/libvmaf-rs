c52d95d52aee9b1f0527aef3aea8479e05d1a20e
Brandon Piña <brandonpi265@gmail.com>
2023-01-22 12:09:15 -0600

    update example with progress bars

--- examples/get_vmaf_scores.rs
+++ examples/get_vmaf_scores.rs
@@ -1,3 +1,4 @@
+use indicatif::{ProgressBar, ProgressStyle};
 use libvmaf_rs::{
     model::{config::ModelConfig, Model},
     video::Video,
@@ -10,19 +11,35 @@ fn main() {
     let reference: Video = Video::new(&"./video/Big Buck Bunny 720P.m4v", 1280, 720).unwrap();
     let distorted: Video = Video::new(&"./video/Big Buck Bunny 720P.m4v", 1280, 720).unwrap();
 
+    let num_frames = reference.len();
+
     let model_config = ModelConfig::default();
     let model = Model::new(model_config, String::from("vmaf_v0.6.1")).unwrap();
 
+    let style =
+        ProgressStyle::with_template("{prefix}: {eta_precise} {wide_bar} [{pos}/{len}]").unwrap();
+
+    let decode_progress = ProgressBar::new(num_frames.try_into().unwrap())
+        .with_prefix("Calculating Vmaf Scores")
+        .with_style(style.clone());
+
+    let get_score_progress = ProgressBar::new(num_frames.try_into().unwrap())
+        .with_prefix("Getting scores")
+        .with_style(style);
+
     let callback = |status: VmafStatus| match status {
-        VmafStatus::Decode => (),
-        VmafStatus::GetScore => (),
+        VmafStatus::Decode => decode_progress.inc(1),
+        VmafStatus::GetScore => get_score_progress.inc(1),
     };
 
     let scores = vmaf
         .get_vmaf_scores(reference, distorted, model, Some(callback))
         .unwrap();
 
-    for (index,score) in scores.into_iter().enumerate() {
-        println!("Score at frame {index}: {score}")
-    }
+    decode_progress.finish();
+    get_score_progress.finish();
+
+    let average: f64 = scores.into_iter().sum::<f64>() / num_frames as f64;
+
+    println!("Pooled VMAF Score: {average}");
 }
